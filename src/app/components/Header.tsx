54b27c07f2c50ea58820879a1bdf91250cd0c9f8
manuel-heav <manuelheav32@gmail.com>
2024-02-14 12:36:53 +0300

    working on header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <main className="flex items-center justify-between py-10 px-32">
       <h1 className="text-2xl font-bold">Logged</h1>
-      <ul className="flex">
-        <li>Download</li>
-        <li>Support</li>
-        <li>Blog</li>
-        <li>Career</li>
+      <ul className="flex items-center justify-between">
+        <li className='px-3'><a href="#">Download</a></li>
+        <li  className='px-3'><a href="#">Support</a></li>
+        <li  className='px-3'><a href="#">Blog</a></li>
+        <li  className='px-3'><a href="#">Career</a></li>
       </ul>
       <h1>Open Logged</h1>
     </main>
